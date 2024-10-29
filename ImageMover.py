0a1c658c1f1b870a58b86e94bef9257e492daa33
slarkslasrkshark <v.muratov@g.nsu.ru>
2024-10-29 18:52:21 +0700

    add start_window

--- ImageMover.py
+++ ImageMover.py
@@ -1,10 +1,10 @@
 from tkinter import *
 from tkinter import filedialog, messagebox
-from PIL import Image, ImageTk, ImageDraw
+from PIL import Image, ImageTk, ImageDraw, ImageFile
 from pathlib import Path
 import shutil
 import pickle
-
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class ImageMoverData:
     def __init__(self, history, main_path, path2, path3, path_ind_next, step, img_names):
@@ -23,61 +23,80 @@ class ImageMover:
         self.end = False
         
         self.selected_index = []
-        self.image_labels = []
+        self.img_labels = []
 
-        self.images_frame = Frame(self.root, width=1300, height=700, borderwidth=0)
+        self.imgs_frame = Frame(self.root, width=1300, height=700, borderwidth=0)
         self.buttons_frame = Frame(self.root, width=350, height=100, borderwidth=0)
         self.save_frame = Frame(self.root, width=1300, height=100, borderwidth=0)
+        self.start_frame = Frame(self.root, width=700, height=700, borderwidth=0)
 
         self.path_ind_next = 0
-        self.images = []
-        self.images_with_frame = []
+        self.imgs = []
+        self.imgs_with_frame = []
         self.img_names = []
         self.default_img = ImageTk.PhotoImage(Image.new('RGB', (224, 224), (255, 255, 255)))
 
-        self.load_button = Button(self.root, text="Загрузить", command=self.load)
+        self.load_button = Button(self.start_frame, text="Загрузить", command=self.load)
         self.load_button.pack()
 
-        self.path_label = Label(self.root, text="Введите путь к основной папке:", font=("Arial", 10) )
+        self.path_label = Label(self.start_frame, text="Введите путь к основной папке:", font=("Arial", 10) )
         self.path_label.pack()
         main_path = StringVar() 
         main_path.set(_main_path) 
-        self.main_path_entry = Entry(self.root, width=50, textvariable=main_path)
+        self.main_path_entry = Entry(self.start_frame, width=50, textvariable=main_path)
         self.main_path_entry.pack()
         
         path2 = StringVar() 
         path2.set(_path2)  
-        self.path_label2 = Label(self.root, text="Введите путь к папке 2:", font=("Arial", 10))
+        self.path_label2 = Label(self.start_frame, text="Введите путь к папке 2:", font=("Arial", 10))
         self.path_label2.pack()
-        self.path_entry2 = Entry(self.root, width=50, textvariable=path2)
+        self.path_entry2 = Entry(self.start_frame, width=50, textvariable=path2)
         self.path_entry2.pack()
         
-        self.path_label3 = Label(self.root, text="Введите путь к папке 3:", font=("Arial", 10))
+        self.path_label3 = Label(self.start_frame, text="Введите путь к папке 3:", font=("Arial", 10))
         self.path_label3.pack()
         path3 = StringVar() 
         path3.set(_path3) 
-        self.path_entry3 = Entry(self.root, width=50, textvariable=path3)
+        self.path_entry3 = Entry(self.start_frame, width=50, textvariable=path3)
         self.path_entry3.pack()
 
-        self.start_button = Button(self.root, text="Начать", font=("Arial", 10, "bold"), command=lambda: self.start("start"))
-        self.start_button.pack()
+        self.start_button = Button(self.start_frame, text="Начать", font=("Arial", 10, "bold"), command=lambda: self.start("start"))
+        self.start_button.pack(pady=10)
+
+        self.start_frame.pack()
+
+        self.end_frame = Frame(self.root, width=500, height=500, borderwidth=0)
+        self.count_label_end = Label(self.end_frame, text="Разметка закончена!", font=("Arial", 20))
+        self.count_label_end.pack(pady=20)
+        self.new_markup = Button(self.end_frame, text="Новая разметка", font=("Arial", 10, "bold"), command=self.new)
+        self.new_markup.pack()
+        self.save_button_end = Button(self.end_frame, text="Сохранить", font=("Arial", 10, "bold"), command=self.save)
+        self.save_button_end.pack()
+        self.cancel_button_end = Button(self.end_frame, text="Отмена", font=("Arial", 10, "bold"), command=self.cancel)
+        self.cancel_button_end.pack()
+
+    def new(self):
+        self.end_frame.pack_forget()
+        del self.history
+        self.__init__(self.root)
 
     def load(self):
         file_path = filedialog.askopenfilename(filetypes=[("Pickle files", "*.pkl"), ("All files", "*.*")])
-        try:
-            with open(file_path, 'rb') as f:
-                Data = pickle.load(f)
-                self.history = Data.history
-                self.main_path = Data.main_path
-                self.path2 = Data.path2
-                self.path3 = Data.path3
-                self.path_ind_next = Data.path_ind_next
-                self.step = Data.step
-                self.img_names = Data.img_names
-            messagebox.showinfo("Успех", "Данные успешно загружены!")
-            self.start("load")
-        except Exception as exc:
-            messagebox.showerror("Ошибка", f"Не удалось загрузить данные: {exc}")
+        if file_path:
+            try:
+                with open(file_path, 'rb') as f:
+                    Data = pickle.load(f)
+                    self.history = Data.history
+                    self.main_path = Data.main_path
+                    self.path2 = Data.path2
+                    self.path3 = Data.path3
+                    self.path_ind_next = Data.path_ind_next
+                    self.step = Data.step
+                    self.img_names = Data.img_names
+                messagebox.showinfo("Успех", "Данные успешно загружены!")
+                self.start("load")
+            except Exception as exc:
+                messagebox.showerror("Ошибка", f"Не удалось загрузить данные: {exc}")
 
     def save(self):
         if self.selected_index or (self.step in self.history and self.history[self.step]):
@@ -85,25 +104,15 @@ class ImageMover:
         else:
             file_path = filedialog.asksaveasfilename(defaultextension=".pkl",
                                                     filetypes=[("Pickle files", "*.pkl"), ("All files", "*.*")])
-            Data = ImageMoverData(self.history, self.main_path, self.path2, self.path3,
-                                  self.path_ind_next, self.step, self.img_names)
-            with open(file_path, 'wb') as f:
-                pickle.dump(Data, f)
-            messagebox.showinfo("Успех", "Данные успешно сохранены!")
+            if file_path:
+                Data = ImageMoverData(self.history, self.main_path, self.path2, self.path3,
+                                    self.path_ind_next, self.step, self.img_names)
+                with open(file_path, 'wb') as f:
+                    pickle.dump(Data, f)
+                messagebox.showinfo("Успех", "Данные успешно сохранены!")
 
     def start(self, flag):
-        self.main_path_entry.pack_forget()
-        self.path_label.pack_forget()
-
-        self.path_entry2.pack_forget()
-        self.path_label2.pack_forget()
-
-        self.path_entry3.pack_forget()
-        self.path_label3.pack_forget()
-
-        self.start_button.pack_forget()
-
-        self.load_button.pack_forget()
+        self.start_frame.pack_forget()
 
         if flag == "start":
             self.step = 1
@@ -128,23 +137,23 @@ class ImageMover:
 
             self.img_names = list(map(lambda x: x.name, self.main_path.glob("*.jpg")))
         
-        self.images_frame.pack(side=TOP)
+        self.imgs_frame.pack(side=TOP)
         self.buttons_frame.pack(side=BOTTOM)
         self.save_frame.pack(side=RIGHT)
 
         self.move_button2 = Button(self.buttons_frame, text=Path(self.path2).name, font=("Arial", 10, "bold"),
-                                   command=lambda folder_index=2: self.move_image_to_folder(folder_index))
-        self.move_button2.pack(side=LEFT)
+                                   command=lambda folder_index=2: self.move_img_to_folder(folder_index))
+        self.move_button2.pack(side=LEFT, pady=10)
 
         self.next_button = Button(self.buttons_frame, text="Далее", font=("Arial", 10, "bold"), command=self.next)
-        self.next_button.pack(side=LEFT)
+        self.next_button.pack(side=LEFT, pady=10)
 
         self.move_button3 = Button(self.buttons_frame, text=Path(self.path3).name, font=("Arial", 10, "bold"),
-                                   command=lambda folder_index=3: self.move_image_to_folder(folder_index))
-        self.move_button3.pack(side=LEFT)
+                                   command=lambda folder_index=3: self.move_img_to_folder(folder_index))
+        self.move_button3.pack(side=LEFT, pady=10)
 
         self.cancel_button = Button(self.buttons_frame, text="Отмена", font=("Arial", 10, "bold"), command=self.cancel)
-        self.cancel_button.pack(side=RIGHT)
+        self.cancel_button.pack(side=LEFT, pady=10)
 
         self.save_button = Button(self.save_frame, text="Сохранить", command=self.save)
         self.save_button.pack(side=RIGHT)
@@ -154,14 +163,14 @@ class ImageMover:
 
         self.count_label = Label(self.root, text=0, font=("Arial", 10))
 
-        self.load_next_images()
+        self.load_next_imgs()
 
     def next(self):
         if self.step not in self.history.keys():
             self.history[self.step] = dict()
         self.step += 1
         self.path_ind_next += 15
-        self.load_next_images()
+        self.load_next_imgs()
 
     def cancel(self):
         if len(self.history) != 0:
@@ -186,11 +195,12 @@ class ImageMover:
 
             if self.end:
                 self.end = False
-                self.move_button2.pack(side=LEFT)
-                self.next_button.pack(side=LEFT)
-                self.move_button3.pack(side=LEFT)
-                
-            self.load_next_images()
+                self.end_frame.pack_forget()
+                self.imgs_frame.pack(side=TOP)
+                self.buttons_frame.pack(side=BOTTOM)
+                self.save_frame.pack(side=RIGHT)
+
+            self.load_next_imgs()
  
     def draw_border(self, img, border_size=7):
         img_b = img.copy()
@@ -201,57 +211,58 @@ class ImageMover:
         draw.rectangle([top_left, bottom_right], outline="red", width=border_size)
         return img_b
 
-    def load_next_images(self):
-        self.images = []
-        self.images_with_frame = []
+    def load_next_imgs(self):
+        self.imgs = []
+        self.imgs_with_frame = []
         self.selected_index = []
         if self.path_ind_next >= len(self.img_names):
-            for label in self.image_labels:
+            for label in self.img_labels:
                 label.destroy()
             self.count_label.destroy()
-            self.move_button2.pack_forget()
-            self.next_button.pack_forget()
-            self.move_button3.pack_forget()
-            self.count_label = Label(self.images_frame, text="Разметка закончена!", font=("Arial", 20))
-            self.count_label.pack(side=TOP)
+
+            self.imgs_frame.pack_forget()
+            self.buttons_frame.pack_forget()
+            self.save_frame.pack_forget()
+            
+            self.end_frame.pack()
             self.end = True
         else:
             self.path_ind = self.path_ind_next
             for i in range(self.path_ind_next, min(self.path_ind + 15, len(self.img_names))):
                 img = Image.open(self.main_path / self.img_names[i])
                 img = img.resize((224, 224))
-                self.images.append(img)
+                self.imgs.append(img)
 
             self.count_label.destroy()
             self.count_label = Label(self.root, text=len(self.img_names) - self.path_ind, font=("Arial", 10))
             self.count_label.pack(side=TOP)
 
-            for label in self.image_labels:
+            for label in self.img_labels:
                 label.destroy()
-            self.image_labels = []
+            self.img_labels = []
             
-            for i in range(min(15, len(self.images))):
-                img = self.images[i]
-                self.images_with_frame.append(ImageTk.PhotoImage(self.draw_border(img)))
-                self.images[i] = ImageTk.PhotoImage(img)
-                label = Label(self.images_frame, image=self.images[i])
+            for i in range(min(15, len(self.imgs))):
+                img = self.imgs[i]
+                self.imgs_with_frame.append(ImageTk.PhotoImage(self.draw_border(img)))
+                self.imgs[i] = ImageTk.PhotoImage(img)
+                label = Label(self.imgs_frame, image=self.imgs[i])
                 if i < 5:
                     label.grid(row=0, column=i)
                 elif i < 10:
                     label.grid(row=1, column=i-5)
                 else:
                     label.grid(row=2, column=i-10)
-                label.bind(f"<Button-1>", lambda e, index=i: self.select_images(e, index))
-                self.image_labels.append(label)
+                label.bind(f"<Button-1>", lambda e, index=i: self.select_imgs(e, index))
+                self.img_labels.append(label)
 
-    def select_images(self, e, index):
-        if self.images[index] != self.default_img:
+    def select_imgs(self, e, index):
+        if self.imgs[index] != self.default_img:
             if index in self.selected_index:
                 self.selected_index.remove(index)
-                self.image_labels[index].config(image=self.images[index])
+                self.img_labels[index].config(image=self.imgs[index])
             else:
                 self.selected_index.append(index)
-                self.image_labels[index].config(image=self.images_with_frame[index])
+                self.img_labels[index].config(image=self.imgs_with_frame[index])
 
     def get_n(self, path):
         names = [name.stem for name in path.glob("*.jpg")]
@@ -260,7 +271,7 @@ class ImageMover:
         names = list(map(lambda x: int(x), names))
         return sorted(names)[-1] + 1
 
-    def move_image_to_folder(self, folder_index):
+    def move_img_to_folder(self, folder_index):
         if self.selected_index:  
             for index in self.selected_index:
 
@@ -273,17 +284,17 @@ class ImageMover:
                     folder_path = self.path3
                     self.n3 += 1
 
-                self.move_image(folder_path, index, n)
-                self.image_labels[index].config(image=self.default_img)
-                self.images[index] = self.default_img
+                self.move_img(folder_path, index, n)
+                self.img_labels[index].config(image=self.default_img)
+                self.imgs[index] = self.default_img
                 self.selected_index = []
             
-    def move_image(self, target_folder, index, n):
+    def move_img(self, target_folder, index, n):
         if self.step not in self.history.keys():
             self.history[self.step] = dict()
 
-        image_name = self.img_names[self.path_ind + index]
-        full_name = self.main_path / image_name
+        img_name = self.img_names[self.path_ind + index]
+        full_name = self.main_path / img_name
         new_full_name = target_folder / f"{n}.jpg"
 
         shutil.move(full_name, new_full_name)
